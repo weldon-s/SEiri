20185be21eeebd2b0a9b29c70dc8224809d62043
Evan Wang <exwang@uwaterloo.ca>
2022-12-01 17:26:54 -0500

    added nlp.py file, function returns a value from 0 to 100 on how close two strings are

---
+++ node/nlp.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from fuzzywuzzy import fuzz
+import spacy
+from collections import Counter
+from string import punctuation
+
+nlp = spacy.load("en_core_web_sm")
+
+def getKeywords(text):
+    result = []
+    pos_tag = ['PROPN', 'ADJ', 'NOUN', 'NUM'] 
+    doc = nlp(text.lower()) 
+    for token in doc:
+        if(token.text in nlp.Defaults.stop_words or token.text in punctuation):
+            continue
+        if(token.pos_ in pos_tag):
+            result.append(token.text)
+
+    return result
+new_text = """
+What was the math one seventeen midterm average?
+"""
+
+# text 1 and 2 can be lists
+def textToText(text1, text2):
+    if type(text1) == str:
+        text1 = getKeywords(text1)
+    if type(text2) == str:
+        text2 = getKeywords(text2)
+    return fuzz.token_sort_ratio(text1, text2)
+
+
+# print(textToText("what was the math one seventeen mean", ["math", "one seventeen"]))
+# print(textToText("what was the math one seventeen mean", ["math", "one fifteen"]))
